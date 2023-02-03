91f4ec559f09e9bd83d9fdad3f752ff4276e0d03
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-02-03 19:39:05 +0200

    Created form for adding catches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,16 @@ import { AppComponent } from './app.component';
 import { LureTableComponent } from '../app/stats-page/lure-table/lure-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatOption } from '@angular/material/core';
+import { MatSliderModule } from '@angular/material/slider';
 import { LakeTableComponent } from '../app/stats-page/lake-table/lake-table.component';
 import { CatchTableComponent } from './stats-page/catch-table/catch-table.component';
+import { CatchFormComponent } from './forms/catch-form/catch-form.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,6 +23,7 @@ import { CatchTableComponent } from './stats-page/catch-table/catch-table.compon
     LureTableComponent,
     LakeTableComponent,
     CatchTableComponent,
+    CatchFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +31,12 @@ import { CatchTableComponent } from './stats-page/catch-table/catch-table.compon
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatSliderModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
